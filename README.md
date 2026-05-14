de35e505999d89f0e808bedd4484566904b9d391
jennywong <jwon1067@ucsc.edu>
2026-05-13 23:51:40 -0700

    assets, title scene, and victory scene update

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # S6_RapidPrototyping
 
 # Assets
-Title Background: https://pixabay.com/illustrations/ai-generated-cabin-forest-witch-hut-8309295/
+Intro Background: https://pixabay.com/illustrations/ai-generated-cabin-forest-witch-hut-8309295/
+
+Menu Background: https://stock.adobe.com/search?filters%5Bcontent_type%3Aphoto%5D=1&filters%5Bcontent_type%3Aillustration%5D=1&filters%5Bcontent_type%3Azip_vector%5D=1&filters%5Bcontent_type%3Avideo%5D=1&filters%5Bcontent_type%3Atemplate%5D=1&filters%5Bcontent_type%3A3d%5D=1&filters%5Bfetch_excluded_assets%5D=1&filters%5Binclude_stock_enterprise%5D=1&filters%5Bcontent_type%3Aimage%5D=1&k=fairy+tale+blank+storybook&order=relevance&limit=100&search_page=1&search_type=usertyped&acp=&aco=fairy+tale+blank+storybook&get_facets=0&asset_id=1052679189
+
 Fairies: https://stock.adobe.com/search?k=fairy+tale+cartoon&asset_id=221990665 
 
+Snails: https://stock.adobe.com/search?filters%5Bcontent_type%3Aphoto%5D=1&filters%5Bcontent_type%3Aillustration%5D=1&filters%5Bcontent_type%3Azip_vector%5D=1&filters%5Bcontent_type%3Avideo%5D=1&filters%5Bcontent_type%3Atemplate%5D=1&filters%5Bcontent_type%3A3d%5D=1&filters%5Bfetch_excluded_assets%5D=1&filters%5Binclude_stock_enterprise%5D=1&filters%5Bcontent_type%3Aimage%5D=1&k=snail+cute+cartoon&order=relevance&search_page=1&search_type=usertyped&acp=&aco=snail+cute+cartoon&get_facets=0&asset_id=1856925888
+
+Roly Poly: https://stock.adobe.com/search?filters%5Bcontent_type%3Aphoto%5D=1&filters%5Bcontent_type%3Aillustration%5D=1&filters%5Bcontent_type%3Azip_vector%5D=1&filters%5Bcontent_type%3Avideo%5D=1&filters%5Bcontent_type%3Atemplate%5D=1&filters%5Bcontent_type%3A3d%5D=1&filters%5Bfetch_excluded_assets%5D=1&filters%5Binclude_stock_enterprise%5D=1&filters%5Bcontent_type%3Aimage%5D=1&k=roly-poly+cartoon&order=relevance&limit=100&search_page=1&search_type=usertyped&acp=&aco=roly-poly+cartoon&get_facets=0&asset_id=1888322589
+
 # Tools
-Adobe Express: remove background to use individual fairies
+Adobe Express: remove background to use individual fairies, snails, and roly poly
 
--- Title.js
+++ Title.js
@@ -3,13 +3,13 @@ class Beveal extends Phaser.GameObjects.Container {
     constructor(scene, x, y) {
         super(scene, x, y);
         // Shapes
-        let topT = scene.add.triangle(0, 0, 73, 23, 148, 73, 223, 23, 0xADB5BD);
-        let leftT = scene.add.triangle(0, 0, 23, 33, 73, 66, 23, 103, 0x495057);
-        let rightT = scene.add.triangle(0, 0, 173, 103, 123, 66, 173, 33, 0x343A40);
-        let bottomT = scene.add.triangle(0, 0, 73, 98, 148, 38, 223, 98, 0x212529);
+        let topT = scene.add.triangle(0, 0, 73, 23, 148, 73, 223, 23, 0x83C5BE);
+        let leftT = scene.add.triangle(0, 0, 23, 33, 73, 66, 23, 103, 0x42999B);
+        let rightT = scene.add.triangle(0, 0, 173, 103, 123, 66, 173, 33, 0x218389);
+        let bottomT = scene.add.triangle(0, 0, 73, 98, 148, 38, 223, 98, 0x006D77);
         
         let rect = scene.add.graphics();
-            rect.fillGradientStyle(0x212529, 0x212529, 0x212529, 0xADB5BD, 1);
+            rect.fillGradientStyle(0x006D77, 0x006D77, 0x006D77, 0x83C5BE, 1);
             rect.fillRect(13, 8, 119, 48);
         
         // Container of shapes
@@ -62,20 +62,20 @@ class Loading extends Phaser.Scene{
         super('loading');
     }
     init(data){
-        this.nextScene = data.next || 'intro';
+        this.nextScene = data.next || 'title';
     }
     preload(){}
     create(){  
     
         // Rectangle frame: rectangle with no fill (centerX, centerY, width, height) border (width, color, opacity)
-        this.add.rectangle(400, 270, 600, 50).setStrokeStyle(8, 0xb6f0deff);
+        this.add.rectangle(400, 270, 600, 50).setStrokeStyle(8, 0x83c5be);
         
         // Loading Text
         this.textObject = this.add.text(
             300,     // x
             150,    // y
             "Loading...", // text
-            { font: "50px Press Start 2P", color: "#b6f0deff" } // size & font, color
+            { font: "50px Press Start 2P", color: "#83c5be" } // size & font, color
         );
 
         // Fade in Scene
@@ -83,7 +83,7 @@ class Loading extends Phaser.Scene{
 
         // Loading Bar
         this.tweens.add({ //bar: rectangle(x, y, fade at x of bar, height, color).setOrigin(Left to Right)
-            targets: this.add.rectangle(110, 270, 0, 30, 0xb6f0deff).setOrigin(0, 0.5), 
+            targets: this.add.rectangle(110, 270, 0, 30, 0x83c5be).setOrigin(0, 0.5), 
             width: 580, 
             alpha: 1, // Opacity: full
             duration: 2000, //duration of action
@@ -100,79 +100,26 @@ class Loading extends Phaser.Scene{
     update(){}
 }
 
-class Intro extends Phaser.Scene{
+class Title extends Phaser.Scene{
     constructor(){
-        super('intro');
+        super('title');
     }
     preload(){
-        this.load.image('background', 'assets/anarkaliart-ai-generated-8309295.png');
+        this.load.image('storyBook', 'assets/AdobeStock_1052679189.jpeg');
+        this.load.image('fairy1', 'assets/greenFairy.png');
     }
     create(){
-        //set background image (WhitePawBlkBg.png)
-        let bg = this.add.image(this.sys.canvas.width / 2, this.sys.canvas.height / 2, 'background');
+        //set background image
+        let bg = this.add.image(this.sys.canvas.width / 2, this.sys.canvas.height / 2, 'storyBook');
         bg.setDisplaySize(this.sys.canvas.width, this.sys.canvas.height);
 
-        // Add Moving Text
-        let title = "Roly Poly:\nTo the End";
-
-        [...title].forEach((letter, i) => {
-
-            let char = this.add.text(
-                75 + (i * 50), // horizontal spacing
-                i % 2 === 0 ? -100 : 600, // alternate top/bottom
-                letter,
-                {
-                    font: "80px Press Start 2P",
-                    color: "#ffffff"
-                }
-            );
-
-            this.tweens.add({
-                targets: char,
-                y: 200,
-                duration: 500,
-                delay: i * 220,
-                ease: 'Back.Out',
-
-                onComplete: () => { // only on last letter
-                    if (i === title.length - 1) {
-                        // 2s delay then loading then menu
-                        this.time.delayedCall(1000, () => {
-                            this.cameras.main.fadeOut(300);
-                            this.scene.start('loading', { next: 'menu' });
-                        });
-                    }
-                }
-            });
-        });
-
-        // Fade in Scene
-        this.cameras.main.fadeIn(500);
-
-    }
-    update(){}
-}
-
-class Menu extends Phaser.Scene{
-    constructor(){
-        super('menu');
-    }
-    preload(){
-        this.load.image('background', 'assets/anarkaliart-ai-generated-8309295.png');
-        this.load.image('fairy', 'assets/greenFairy.png');
-    }
-    create(){
-        //set background image (WhitePawBlkBg.png)
-        let bg = this.add.image(this.sys.canvas.width / 2, this.sys.canvas.height / 2, 'background');
-        bg.setDisplaySize(this.sys.canvas.width, this.sys.canvas.height);
-
-        //cat picture
-        this.imageObject = this.add.image(650, 250, 'fairy');
+        //green fairy picture
+        this.imageObject = this.add.image(550, 275, 'fairy1');
         this.imageObject.setScale(0.48);
 
         //Add buttons
         new Button(this, 160, 180, 'Start', () => {
-            this.scene.start('loading'); 
+            this.scene.start('victory'); 
         });
 
         new Button(this, 160, 260, 'Exit', () => {});
@@ -180,14 +127,105 @@ class Menu extends Phaser.Scene{
         new Button(this, 160, 340, 'Menu', () => {});
 
         this.textObject = this.add.text(
-            80,     // x
+            120,     // x
             100,    // y
-            "Roly Poly: To the End",
+            "Roly Poly:   To the End",
             { font: "60px Press Start 2P", color: "#ffffff" } // white text
         );
 
         // Fade in Scene
         this.cameras.main.fadeIn(500);
+    }
+    update(){}
+}
+
+class Victory extends Phaser.Scene{
+    constructor(){
+        super('victory');
+    }
+    preload(){
+        //background image
+        this.load.image('background', 'assets/anarkaliart-ai-generated-8309295.png');
+        //fairy images
+        this.load.image('fairy1', 'assets/greenFairy.png');
+        this.load.image('fairy2', 'assets/pinkFairy.png');
+        this.load.image('fairy3', 'assets/blueFairy.png');
+        this.load.image('fairy4', 'assets/tealFairy.png');
+        this.load.image('fairy5', 'assets/purpleFairy.png');
+        //snail images
+        this.load.image('snail1', 'assets/brownSnail.png');
+        this.load.image('snail2', 'assets/greySnail.png');
+        //roly poly image
+        this.load.image('rolyPoly', 'assets/rolyPoly.png');
+        //banner image
+        this.load.image('banner', 'assets/AdobeStock_415886775.png');
+    }
+    create(){
+        //set background image
+        let bg = this.add.image(this.sys.canvas.width / 2, this.sys.canvas.height / 2, 'background');
+        bg.setDisplaySize(this.sys.canvas.width, this.sys.canvas.height);
+
+        // add fairies in an array
+        this.fairies = [];
+
+        this.fairies.push(this.add.image(350, 100, 'fairy4'));
+        this.fairies.push(this.add.image(650, 300, 'fairy1'));
+        this.fairies.push(this.add.image(100, 200, 'fairy5'));
+        this.fairies.push(this.add.image(520, 170, 'fairy2'));
+        this.fairies.push(this.add.image(250, 350, 'fairy3'));
+
+        // add snails
+        this.imageObject = this.add.image(140, 400, 'snail1');
+        this.imageObject.setScale(0.2);
+
+        this.imageObject = this.add.image(500, 400, 'snail2');
+        this.imageObject.setScale(0.2);
+
+        // add roly poly
+        this.imageObject = this.add.image(370, 420, 'rolyPoly');
+        this.imageObject.setScale(0.35);
+
+        // Add moving fairies
+        let i = 0; // index for fairies
+
+        this.fairies.forEach(fairy => {
+            fairy.setAlpha(0); // Invisible fairies
+            fairy.setScale(0.4); // full size fairies
+            fairy.scaleX = 0.4; // ensure consistent start
+        });
+
+        let playFairy = () => {
+            if (i >= this.fairies.length){ 
+                   this.imageObject = this.add.image(380, 250, 'banner').setScale(0.12);
+                return; 
+            }
+
+            let fairy = this.fairies[i];
+
+            this.tweens.add({ // fairy pop in
+                targets: fairy,
+                alpha: 1,
+                scale: 0.4,
+                duration: 500,
+                ease: 'Back.out(2)',
+
+                onComplete: () => {
+                    this.tweens.add({ // horizontal spin
+                        targets: fairy,
+                        scaleX: 0, 
+                        duration: 100,
+                        yoyo: true,
+                        ease: 'Sine.inOut',
+
+                        onComplete: () => { // move to next fairy
+                            i++;
+                            playFairy();
+                        }
+                    });
+                }
+            });
+        };
+        playFairy();
     }
     update(){}
 }
@@ -200,8 +238,8 @@ let config = {
         width: 800, 
         height: 500, 
     },
-    backgroundColor: 0x76bdafff,
-    scene: [Loading, Intro, Menu],
+    backgroundColor: 0x006d77,
+    scene: [Loading, Title, Victory],
 }
 
 let game = new Phaser.Game(config);
